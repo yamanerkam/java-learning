16ef71935bf5779dfd75aa1813eaf14777f28786
Kam <erkamyaman35@gmail.com>
2024-07-15 19:19:12 +0300

    string-methods

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello");
+    /*    System.out.println("Hello");
         int money = 20;
         money = 40;
         float f = 10.5f;
@@ -9,24 +9,56 @@ public class Main {
         char abc = 'A';
         boolean isAllowed = true;
         System.out.println("Money: " + money);
-        if(isAllowed){
+        if (isAllowed) {
             System.out.println(abc);
-        }else{
+        } else {
             System.out.println("not allowed");
         }
-        int [] grades = {0,5,10,20,30,40,50,60,70,80,90,100};
-        for(int grade:grades){
-            System.out.println(grade*10);
+        int[] grades = {0, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        for (int grade : grades) {
+            System.out.println(grade * 10);
 
         }
 
         MyClass classOne = new MyClass();
-        System.out.println(classOne.x);
+        System.out.println(classOne.x); */
 
+        String Kam = new String("Erkam Yaman");
+        System.out.println(Kam);
+        System.out.println(Kam.length());
+        System.out.println(Kam.indexOf('k'));
+        System.out.println(Kam.charAt(0));
+        System.out.println(Kam.substring(6));
+
+        String str = "Hello, World! ";
+        System.out.println(str);
+        // Length of the string
+        int length = str.length();
+        System.out.println("Length: " + length); // 15
+
+        // Trimmed string
+        String trimmed = str.trim();
+        System.out.println("Trimmed: " + trimmed); // "Hello, World!"
+
+        // Substring
+        String sub = trimmed.substring(7);
+        System.out.println("Substring: " + sub); // "World!"
+
+        // Upper case
+        String upper = trimmed.toUpperCase();
+        System.out.println("Upper case: " + upper); // "HELLO, WORLD!"
+
+        // Replace
+        String replaced = trimmed.replace("World", "Java");
+        System.out.println("Replaced: " + replaced); // "Hello, Java!"
+
+        // Split
+        String[] parts = trimmed.split(", ");
+        System.out.println("Split: " + java.util.Arrays.toString(parts));
     }
 }
 
 class MyClass {
-int x =10;
+    int x = 10;
 
 }
